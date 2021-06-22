2093a3177d65123322af16af3ce416c57d0c5086
Jessica Mulein <jessica@mulein.com>
2021-06-22 15:34:03 -0700

    update todo in redundancy contract

--- BrightChain/Enumerations/RedundancyContractType.cs
+++ BrightChain/Enumerations/RedundancyContractType.cs
@@ -2,6 +2,7 @@
 {
     /// <summary>
     /// Determines the minimum replication effort required/desired for a given block
+    /// TODO: these were just a thought.
     /// </summary>
     public enum RedundancyContractType
     {
