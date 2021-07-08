52ca5ffdf6c5122d9f3b8bfb4de1cfb10d24a316
Jessica Mulein <jessica@mulein.com>
2021-07-08 15:07:56 -0700

    contexts everywhere

--- BrightChain.API/Areas/Identity/IdentityHostingStartup.cs
+++ BrightChain.API/Areas/Identity/IdentityHostingStartup.cs
@@ -1,8 +1,10 @@
 ﻿using BrightChain.EntityFrameworkCore.Data;
+using BrightChain.EntityFrameworkCore.Extensions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 [assembly: HostingStartup(typeof(BrightChain.API.Areas.Identity.IdentityHostingStartup))]
 namespace BrightChain.API.Areas.Identity
@@ -11,9 +13,8 @@ namespace BrightChain.API.Areas.Identity
     {
         public void Configure(IWebHostBuilder builder) => builder.ConfigureServices((context, services) =>
         {
-            services.AddDbContext<BrightChainIdentityDbContext>(options =>
-                options.UseSqlServer(
-                    context.Configuration.GetConnectionString("BrightChainAPIContextConnection")));
+            services.AddDbContext<BrightChainIdentityDbContext>((p, o) =>
+                    o.UseBrightChainDatabase(databaseName: Guid.NewGuid().ToString()));
 
             services.AddDefaultIdentity<BrightChainUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<BrightChainIdentityDbContext>();
