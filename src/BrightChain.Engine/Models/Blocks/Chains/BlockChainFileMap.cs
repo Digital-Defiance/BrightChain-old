MaxMood96/BrightChain

start laying direction for key service/users/user keys

src/BrightChain.Engine/Models/Blocks/Chains/BlockChainFileMap.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using BrightChain.Engine.Exceptions;
 using BrightChain.Engine.Models.Blocks.DataObjects;
 
 namespace BrightChain.Engine.Models.Blocks.Chains
@@ -8,47 +14,75 @@ namespace BrightChain.Engine.Models.Blocks.Chains
     /// Represents a virtual map of all the contituent tuple-sets/blocks in a given source/reconstructed file. These cannot themselves be committed to disk
     /// The block datas may not actually be loaded in memory, but the appropriate blocks will be loaded (all non-local will be pulled into the cache first) relative to their access offsets.
     /// source file -> blockchainfilemap -> commit
-    /// pull blocks from pool -> blockchainfilemap -> read
+    /// pull blocks from pool -> blockchainfilemap -> read.
     /// </summary>
-    public class BlockChainFileMap : ConstituentBlockListBlock
+    public class BlockChainFileMap
     {
-        public IEnumerable<TupleStripe> Tuples { get; }
-        public BlockChainFileMap(ConstituentBlockListBlockParams blockParams) : base(blockParams: blockParams)
+        private IAsyncEnumerable<TupleStripe> TupleStripes { get; set; }
+
+        public ConstituentBlockListBlock ConstituentBlockListBlock { get; }
+
+        public static async IAsyncEnumerable<IEnumerable<T>> TakeIntoGroupsOf<T>(IEnumerable<T> list, int parts)
         {
+            var result = list.Select((item, index) => new { index, item })
+                       .GroupBy(x => x.index % parts)
+                       .Select(x => x.Select(y => y.item));
+            yield return (IEnumerable<T>)result;
+            yield break;
         }
 
-        public static IEnumerable<TupleStripe> ReconstructTupleStripes(IEnumerable<Block> constituentBlocks)
+        public BlockChainFileMap(ConstituentBlockListBlock cblBlock, IAsyncEnumerable<TupleStripe> tupleStripes = null)
         {
-            // var stripe = constituentBlocks.Take(BlockWhitener.TupleCount);
-            throw new NotImplementedException();
+            this.ConstituentBlockListBlock = cblBlock;
+            this.TupleStripes = tupleStripes;
         }
 
-        public BlockChainFileMap(ConstituentBlockListBlock cblBlock, IEnumerable<TupleStripe> tuples = null)
-        : base(
-              blockParams: new ConstituentBlockListBlockParams(
-                  blockParams: new TransactableBlockParams(
-                  cacheManager: cblBlock.CacheManager,
-                  blockParams: new BlockParams(
-                      blockSize: cblBlock.BlockSize,
-                      requestTime: cblBlock.StorageContract.RequestTime,
-                      keepUntilAtLeast: cblBlock.StorageContract.KeepUntilAtLeast,
-                      redundancy: cblBlock.RedundancyContract.RedundancyContractType,
-                      allowCommit: cblBlock.AllowCommit,
-                      privateEncrypted: cblBlock.PrivateEncrypted)),
-              finalDataHash: cblBlock.SourceId,
-              totalLength: cblBlock.TotalLength,
-              constituentBlocks: cblBlock.ConstituentBlocks))
+        private BlockChainFileMap()
         {
-            Tuples = (tuples is null) ? tuples : ReconstructTupleStripes(cblBlock.ConstituentBlocks);
+
         }
 
-        /// <summary>
-        /// TODO: extend
-        /// </summary>
-        /// <returns></returns>
-        public new bool Validate()
+        public async IAsyncEnumerable<TupleStripe> ReconstructTupleStripes()
         {
-            return base.Validate();
+            Block[] listBlocks = (Block[])this.ConstituentBlockListBlock.ConstituentBlocks;
+            if ((listBlocks.Length % this.ConstituentBlockListBlock.TupleCount) != 0)
+            {
+                throw new BrightChainException("CBL length is not a multiple of the tuple count");
+            }
+
+            var tupleGroups = TakeIntoGroupsOf(listBlocks, this.ConstituentBlockListBlock.TupleCount);
+            await foreach (var tupleGroup in tupleGroups)
+            {
+                yield return new TupleStripe(this.ConstituentBlockListBlock.TupleCount, this.ConstituentBlockListBlock.BlockSize, tupleGroup);
+            }
+
+            yield break;
+        }
+
+        public async IAsyncEnumerable<Block> ConsolidateTuplesToChainAsyc()
+        {
+            await foreach (TupleStripe tupleStripe in (this.TupleStripes is null) ? this.ReconstructTupleStripes() : this.TupleStripes)
+            {
+                yield return tupleStripe.Consolidate();
+            }
+
+            yield break;
+        }
+
+        public static async IAsyncEnumerator<byte> ReadValidatedChainToBytes(IAsyncEnumerable<Block> source)
+        {
+            await foreach (var block in source)
+            {
+                if (!block.Validate())
+                {
+                    throw new BrightChainValidationEnumerableException(block.ValidationExceptions, block.Id.ToString());
+                }
+
+                foreach (byte b in block.Data.ToArray())
+                {
+                    yield return b;
+                }
+            }
         }
     }
 }

src/BrightChain.Engine/Models/Blocks/DataObjects/ConstituentBlockListBlockParams.cs
@@ -4,18 +4,29 @@ namespace BrightChain.Engine.Models.Blocks.DataObjects
 {
     public class ConstituentBlockListBlockParams : TransactableBlockParams
     {
-        public BlockHash FinalDataHash;
-        public ulong TotalLength;
-        public IEnumerable<Block> ConstituentBlocks;
+        public readonly DataHash SourceId;
+        public readonly long TotalLength;
+        public readonly IEnumerable<Block> ConstituentBlocks;
 
-        public ConstituentBlockListBlockParams(TransactableBlockParams blockParams, BlockHash finalDataHash, ulong totalLength, IEnumerable<Block> constituentBlocks)
+        /// <summary>
+        /// Hash of the sum bytes of the segment of the file contained in this CBL when assembled in order.
+        /// </summary>
+        public SegmentHash SegmentId;
+        public readonly BlockHash Previous = null;
+        public readonly BlockHash Next = null;
+
+        public ConstituentBlockListBlockParams(TransactableBlockParams blockParams, DataHash sourceId, SegmentHash segmentHash, long totalLength, IEnumerable<Block> constituentBlocks, BlockHash previous = null, BlockHash next = null)
        : base(
              cacheManager: blockParams.CacheManager,
+             allowCommit: blockParams.AllowCommit,
              blockParams: blockParams)
         {
-            FinalDataHash = finalDataHash;
-            TotalLength = totalLength;
-            ConstituentBlocks = constituentBlocks;
+            this.SourceId = sourceId;
+            this.TotalLength = totalLength;
+            this.ConstituentBlocks = constituentBlocks;
+            this.SegmentId = segmentHash;
+            this.Previous = previous;
+            this.Next = next;
         }
     }
 }

Digital-Defiance/BrightChain-old

src/BrightChain.Engine/Models/Blocks/Chains/BlockChainFileMap.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using BrightChain.Engine.Exceptions;
 using BrightChain.Engine.Models.Blocks.DataObjects;
 
 namespace BrightChain.Engine.Models.Blocks.Chains
@@ -8,47 +14,76 @@ namespace BrightChain.Engine.Models.Blocks.Chains
     /// Represents a virtual map of all the contituent tuple-sets/blocks in a given source/reconstructed file. These cannot themselves be committed to disk
     /// The block datas may not actually be loaded in memory, but the appropriate blocks will be loaded (all non-local will be pulled into the cache first) relative to their access offsets.
     /// source file -> blockchainfilemap -> commit
-    /// pull blocks from pool -> blockchainfilemap -> read
+    /// pull blocks from pool -> blockchainfilemap -> read.
     /// </summary>
-    public class BlockChainFileMap : ConstituentBlockListBlock
+    public class BlockChainFileMap
     {
-        public IEnumerable<TupleStripe> Tuples { get; }
-        public BlockChainFileMap(ConstituentBlockListBlockParams blockParams) : base(blockParams: blockParams)
+        private IAsyncEnumerable<TupleStripe> TupleStripes { get; set; }
+
+        public ConstituentBlockListBlock ConstituentBlockListBlock { get; }
+
+        public static async IAsyncEnumerable<IEnumerable<T>> TakeIntoGroupsOf<T>(IEnumerable<T> list, int parts)
         {
+            var result = list.Select((item, index) => new { index, item })
+                       .GroupBy(x => x.index % parts)
+                       .Select(x => x.Select(y => y.item));
+            yield return (IEnumerable<T>)result;
+            yield break;
         }
 
-        public static IEnumerable<TupleStripe> ReconstructTupleStripes(IEnumerable<Block> constituentBlocks)
+        public BlockChainFileMap(ConstituentBlockListBlock cblBlock, IAsyncEnumerable<TupleStripe> tupleStripes = null)
         {
-            // var stripe = constituentBlocks.Take(BlockWhitener.TupleCount);
-            throw new NotImplementedException();
+            this.ConstituentBlockListBlock = cblBlock;
+            this.TupleStripes = tupleStripes;
         }
 
-        public BlockChainFileMap(ConstituentBlockListBlock cblBlock, IEnumerable<TupleStripe> tuples = null)
-        : base(
-              blockParams: new ConstituentBlockListBlockParams(
-                  blockParams: new TransactableBlockParams(
-                  cacheManager: cblBlock.CacheManager,
-                  blockParams: new BlockParams(
-                      blockSize: cblBlock.BlockSize,
-                      requestTime: cblBlock.StorageContract.RequestTime,
-                      keepUntilAtLeast: cblBlock.StorageContract.KeepUntilAtLeast,
-                      redundancy: cblBlock.RedundancyContract.RedundancyContractType,
-                      allowCommit: cblBlock.AllowCommit,
-                      privateEncrypted: cblBlock.PrivateEncrypted)),
-              finalDataHash: cblBlock.SourceId,
-              totalLength: cblBlock.TotalLength,
-              constituentBlocks: cblBlock.ConstituentBlocks))
+        private BlockChainFileMap()
         {
-            Tuples = (tuples is null) ? tuples : ReconstructTupleStripes(cblBlock.ConstituentBlocks);
+
         }
 
-        /// <summary>
-        /// TODO: extend
-        /// </summary>
-        /// <returns></returns>
-        public new bool Validate()
+        public async IAsyncEnumerable<TupleStripe> ReconstructTupleStripes()
         {
-            return base.Validate();
+            BlockHash[] listBlocks = (BlockHash[])this.ConstituentBlockListBlock.ConstituentBlocks;
+            if ((listBlocks.Length % this.ConstituentBlockListBlock.TupleCount) != 0)
+            {
+                throw new BrightChainException("CBL length is not a multiple of the tuple count");
+            }
+
+            var tupleGroups = TakeIntoGroupsOf(listBlocks, this.ConstituentBlockListBlock.TupleCount);
+            await foreach (var tupleGroup in tupleGroups)
+            {
+                throw new NotImplementedException();
+                //yield return new TupleStripe(this.ConstituentBlockListBlock.TupleCount, this.ConstituentBlockListBlock.BlockSize, tupleGroup);
+            }
+
+            yield break;
+        }
+
+        public async IAsyncEnumerable<Block> ConsolidateTuplesToChainAsyc()
+        {
+            await foreach (TupleStripe tupleStripe in (this.TupleStripes is null) ? this.ReconstructTupleStripes() : this.TupleStripes)
+            {
+                yield return tupleStripe.Consolidate();
+            }
+
+            yield break;
+        }
+
+        public static async IAsyncEnumerator<byte> ReadValidatedChainToBytes(IAsyncEnumerable<Block> source)
+        {
+            await foreach (var block in source)
+            {
+                if (!block.Validate())
+                {
+                    throw new BrightChainValidationEnumerableException(block.ValidationExceptions, block.Id.ToString());
+                }
+
+                foreach (byte b in block.Data.ToArray())
+                {
+                    yield return b;
+                }
+            }
         }
     }
 }

src/BrightChain.Engine/Models/Blocks/DataObjects/ConstituentBlockListBlockParams.cs
@@ -4,18 +4,29 @@ namespace BrightChain.Engine.Models.Blocks.DataObjects
 {
     public class ConstituentBlockListBlockParams : TransactableBlockParams
     {
-        public BlockHash FinalDataHash;
-        public ulong TotalLength;
-        public IEnumerable<Block> ConstituentBlocks;
+        public readonly DataHash SourceId;
+        public readonly long TotalLength;
+        public readonly IEnumerable<BlockHash> ConstituentBlocks;
 
-        public ConstituentBlockListBlockParams(TransactableBlockParams blockParams, BlockHash finalDataHash, ulong totalLength, IEnumerable<Block> constituentBlocks)
+        /// <summary>
+        /// Hash of the sum bytes of the segment of the file contained in this CBL when assembled in order.
+        /// </summary>
+        public SegmentHash SegmentId;
+        public readonly BlockHash Previous = null;
+        public readonly BlockHash Next = null;
+
+        public ConstituentBlockListBlockParams(TransactableBlockParams blockParams, DataHash sourceId, SegmentHash segmentHash, long totalLength, IEnumerable<BlockHash> constituentBlocks, BlockHash previous = null, BlockHash next = null)
        : base(
              cacheManager: blockParams.CacheManager,
+             allowCommit: blockParams.AllowCommit,
              blockParams: blockParams)
         {
-            FinalDataHash = finalDataHash;
-            TotalLength = totalLength;
-            ConstituentBlocks = constituentBlocks;
+            this.SourceId = sourceId;
+            this.TotalLength = totalLength;
+            this.ConstituentBlocks = constituentBlocks;
+            this.SegmentId = segmentHash;
+            this.Previous = previous;
+            this.Next = next;
         }
     }
 }