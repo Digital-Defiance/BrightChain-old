428ca4a6375e59bb661663dfb4f48ba04117f05b
Jessica Mulein <jessica@brightchain.net>
2021-09-06 14:05:14 -0700

    refine brightmail

--- src/BrightChain.Engine/Models/Blocks/BrightMail.cs
+++ src/BrightChain.Engine/Models/Blocks/BrightMail.cs
@@ -1,12 +1,12 @@
 ﻿namespace BrightChain.Engine.Models.Blocks
 {
     using BrightChain.Engine.Models.Entities;
+    using BrightChain.Engine.Models.Hashes;
 
     public class BrightMail : BrightBlockMessage
     {
         private readonly IEnumerable<string> Headers;
-        private readonly string Body;
-        private readonly IEnumerable<BrightenedBlock> Attachments;
+        private readonly IEnumerable<BlockHash> Attachments;
         private readonly bool recipientBcc;
 
         private bool _disposedValue;
