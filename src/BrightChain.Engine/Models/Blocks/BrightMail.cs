99c2e5f0dac5e6d7d480930a681c929c5a5dc234
Jessica Mulein <jessica@brightchain.net>
2021-09-06 13:58:32 -0700

    base messages on block message

--- src/BrightChain.Engine/Models/Blocks/BrightMail.cs
+++ src/BrightChain.Engine/Models/Blocks/BrightMail.cs
@@ -2,54 +2,13 @@
 {
     using BrightChain.Engine.Models.Entities;
 
-    public class BrightMail : IDisposable
+    public class BrightMail : BrightBlockMessage
     {
-        private readonly Guid Id;
         private readonly IEnumerable<string> Headers;
-        private readonly Agent Sender;
-        private readonly Agent Recipient;
-        private readonly string Subject;
         private readonly string Body;
         private readonly IEnumerable<BrightenedBlock> Attachments;
-        private bool recipientBcc;
-        private readonly DateTime Sent;
-        private readonly DateTime? Read;
-        private readonly bool Deleted;
+        private readonly bool recipientBcc;
 
         private bool _disposedValue;
-
-        private BrightMail()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!_disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects)
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
-                _disposedValue = true;
-            }
-        }
-
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~BrightMail()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
-
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
     }
 }
--- src/BrightChain.Engine/Models/Blocks/BrightMessage.cs
+++ src/BrightChain.Engine/Models/Blocks/BrightMessage.cs
@@ -2,15 +2,7 @@
 {
     using BrightChain.Engine.Models.Entities;
 
-    public class BrightMessage
+    public class BrightMessage : BrightBlockMessage
     {
-        private readonly Guid Id;
-        private readonly Agent Sender;
-        private readonly Agent Recipient;
-        private readonly string Body;
-        private readonly DateTime Sent;
-        private readonly DateTime? Read;
-        private readonly bool Deleted;
-        private readonly Guid Thread;
     }
 }
