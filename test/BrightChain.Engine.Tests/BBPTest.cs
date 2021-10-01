f5bff21bde0008181422af4b9254db45563771b0
Jessica Mulein <jessica@mulein.com>
2021-09-30 18:46:50 -0700

    add foundations for pi based storage...

---
+++ test/BrightChain.Engine.Tests/BBPTest.cs
@@ -0,0 +1,15 @@
+﻿namespace BrightChain.Engine.Tests
+{
+    using System;
+    using BBP;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BBPTest
+    {
+        [TestMethod]
+        public void TestBBP()
+        {
+        }
+    }
+}
