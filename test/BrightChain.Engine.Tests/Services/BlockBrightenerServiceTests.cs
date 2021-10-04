65691a6b81fccc99a6ea76df448f7e7cb2eb6f33
Jessica Mulein <jessica@mulein.com>
2021-10-04 15:11:17 -0700

    new test stub

---
+++ test/BrightChain.Engine.Tests/Services/BlockBrightenerServiceTests.cs
@@ -0,0 +1,52 @@
+﻿namespace BrightChain.Engine.Tests.Services
+{
+    using BrightChain.Engine.Models.Blocks;
+    using BrightChain.Engine.Models.Blocks.Chains;
+    using BrightChain.Engine.Services;
+    using BrightChain.Engine.Services.CacheManagers.Block;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using System;
+
+    [TestClass]
+    public class BlockBrightenerServiceTests
+    {
+        private MockRepository mockRepository;
+
+        private Mock<BrightenedBlockCacheManagerBase> mockBrightenedBlockCacheManagerBase;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+            this.mockBrightenedBlockCacheManagerBase = this.mockRepository.Create<BrightenedBlockCacheManagerBase>();
+        }
+
+        private BlockBrightenerService CreateService()
+        {
+            return new BlockBrightenerService(
+                this.mockBrightenedBlockCacheManagerBase.Object);
+        }
+
+        [TestMethod]
+        public void Brighten_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var service = this.CreateService();
+            IdentifiableBlock identifiableBlock = null;
+            BrightenedBlock[] randomizersUsed = null;
+            TupleStripe brightenedStripe = default(global::BrightChain.Engine.Models.Blocks.Chains.TupleStripe);
+
+            // Act
+            var result = service.Brighten(
+                identifiableBlock,
+                out randomizersUsed,
+                out brightenedStripe);
+
+            // Assert
+            Assert.Fail();
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
