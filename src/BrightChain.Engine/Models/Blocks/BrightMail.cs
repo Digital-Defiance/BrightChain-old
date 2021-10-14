2b5066ba081252a4e70d3cbe198bbbdbad2372c9
Jessica Mulein <jessica@brightchain.org>
2021-10-13 19:53:31 -0700

    add todo note

--- src/BrightChain.Engine/Models/Blocks/BrightMail.cs
+++ src/BrightChain.Engine/Models/Blocks/BrightMail.cs
@@ -3,6 +3,9 @@
     using System.Collections.Generic;
     using BrightChain.Engine.Models.Hashes;
 
+    /// <summary>
+    /// TODO: This needs a total rethink/redo. Decide whether/where to store IEnumerable<(RecipientType, BrightChainAgent)>
+    /// </summary>
     public record BrightMail : BrightMessage
     {
         private readonly IEnumerable<string> Headers;
