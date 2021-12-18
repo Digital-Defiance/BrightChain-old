48f503c9b4a6b59b28fa2435e944afc2f08bdb90
Jessica Mulein <jessica@mulein.com>
2021-12-18 12:53:42 -0800

    better define state

--- src/BrightChain.Engine/Enumerations/TransactionStatus.cs
+++ src/BrightChain.Engine/Enumerations/TransactionStatus.cs
@@ -9,31 +9,37 @@ public enum TransactionStatus
 
     /// <summary>
     /// This block has not been written to disk.
+    /// Memory Dictionary only.
     /// </summary>
     Uncommitted,
 
     /// <summary>
     /// This block explicitly rolled back. Do not write.
+    /// Effectively dropped. Removed from fasterKV. Memory dictionary only.
     /// </summary>
     RolledBackDoNotWrite,
 
     /// <summary>
     /// Another block was rolled back, causing this block to need to be rewritten.
+    /// Memory dictionary only, pending re-add to FasterKV.
     /// </summary>
     RolledBackRewrite,
 
     /// <summary>
-    /// Written to disk but transaction not confirmed/completed.
+    /// Written to disk but transaction not confirmed/completed. FasterKV + MemoryDict
     /// </summary>
     WrittenUnconfirmed,
 
     /// <summary>
     /// Confirmed written to disk and session completed successfully.
+    /// Removed from MemoryDictionary. FasterKV only.
     /// </summary>
     Committed,
 
     /// <summary>
-    /// Confirmed removed from disk and session completed successfully. 
+    /// Confirmed removed from disk and session completed successfully.
+    /// Removed from memory dictionary and fasterkv.
+    /// Only copies remaining should be variable references to original object.
     /// </summary>
     DroppedCommitted,
 }
